63e40a2ee5153d8fc97e3d968b38cdc9a67f7af5
root <root@vps00.aleph.engineering>
2019-02-12 22:06:37 +0100

    Correct message on email

--- create_user.sh
+++ create_user.sh
@@ -1,3 +1,3 @@
 docker run -v /root/docker-openvpn/vpn-data:/etc/openvpn --log-driver=none --rm -it myownvpn easyrsa build-client-full $1 nopass
 docker run -v /root/docker-openvpn/vpn-data:/etc/openvpn --log-driver=none --rm -it myownvpn ovpn_getclient $1 > profiles/$1.ovpn
-echo "Use this configuration in only one device" | mutt -a profiles/$1.ovpn -s "Your phone VPN configuration" -- $2
+echo "Use this configuration in only one device" | mutt -a profiles/$1.ovpn -s "Your VPN configuration" -- $2
